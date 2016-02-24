149ee8f96c239872956fa57584939444870aa8fd
Leandro David <ldavid@br.ibm.com>
2016-02-24 04:27:07 -0300

    updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var http=require("http");
 var bl=require('bl');
-app.use(express.static(__dirname+'/public')); 
+app.use(express.static(__dirname+'/public'));
 
 
 
@@ -38,20 +38,33 @@ app.get('/getRandomVideo', function(req,res){
 
 function loadAllVideosFromBackend(){
 	videos=[];
-	var pagesCount=(391489/20)-1;
-	for (var page=0;page<pagesCount;page++){
-		loadVideosFromBackendPage(page);
+	//var pagesCount=(621191/20)-1;
+
+	 var pagesCount=(20000/20)-1;
+	//var pagesCount =10;
+		console.log("INITIAL PAGE COUNT "+ pagesCount);
+	for (var innerpage=0;innerpage<pagesCount;innerpage++){
+		var loadFunction = function(page){
+			return function(data){
+				loadVideosFromBackendPage(page);
+			}
+
+		}
+		setTimeout(loadFunction(innerpage),innerpage*100);
+
 	}
 }
 
 
 function loadVideosFromBackendPage(page){
 	try{
+		var that = this;
 		var url="http://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&page="+page
 		console.log("request videos to page "+ page);
 		//console.log("url:"+url);
-		var req = http.get(url, function(res) {	
+		var req = http.get(url, function(res) {
 	  		//console.log("Response Status: " + res.statusCode);
+				console.log("Response to page "+ page);
 	  		res.pipe(bl(function (err, data) {
 	  			try{
 		  			if(err){
@@ -64,17 +77,17 @@ function loadVideosFromBackendPage(page){
 					// console.log("Video response count:"+count);
 					for (var i=0;i<length;i++){
 						videos.push(videosResponse.videos[i].video.video_id);
-						// console.log("Added video id:"+videosResponse.videos[i].video.video_id);
-						// console.log("Videos list size:"+videos.length);
+						//  console.log("Added video id:"+videosResponse.videos[i].video.video_id);
+						//  console.log("Videos list size:"+videos.length);
 					}
-					console.log("Added videos from page:"+page);
-					console.log("Videos list size:"+videos.length);
+					// console.log("Added videos from page:"+that.page);
+					 console.log("Videos list size:"+videos.length);
 				}
 				catch(error){
 					return console.error("Exception retrieving data pipe from backend "+err);
 				}
 			}));
-	  		
+
 		});
 	}
 	catch(err){
